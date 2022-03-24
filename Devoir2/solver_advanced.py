bc9fd4a4ca67284f86347759b2917dcb43611ab9
Guyluc <theo.delbecq@free.fr>
2022-03-24 04:54:58 +0100

    commentaires + finitions

--- Devoir2/solver_advanced.py
+++ Devoir2/solver_advanced.py
@@ -1,5 +1,7 @@
-import solver_grasp as sg
+# Le meilleur solveur retenu est ITS
+# Les meilleurs résultats poru chaque instance sont dans le dossier best_results
+import solver_iterated_tabu as ITS
 
 
 def solve(mother):
-    return sg.solve(mother)
+    return ITS.solve(mother)
